a423b4e16c00285669656bd397cfb75a74c416f0
unknown <ommarkhawaja1@gmail.com>
2022-09-15 21:19:20 -0400

    typo

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use(require('./Controllers/'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
